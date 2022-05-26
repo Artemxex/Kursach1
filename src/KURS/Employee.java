dabcf9b010eb22e17b559badd7806018a1d3d09a
Artemxex <Artem.363636@gmail.com>
2022-05-26 22:41:31 +0300

    first

---
+++ src/KURS/Employee.java
@@ -0,0 +1,98 @@
+package KURS;
+
+public class Employee {
+    public String employeeName;
+    public int employeeDepartment;
+    public double employeeSalary;
+    public int id;
+    public static int count = 0;
+    public double employeeSalarySum = 0;
+    public static double SalarySum;
+    public static double minSalary = 99999999;
+    public static double maxSalary = 0;
+    public static double average;
+
+    public Employee(String employeeName, int employeeDepartment, double employeeSalary) {
+        this.employeeName = employeeName;
+        this.employeeDepartment = employeeDepartment;
+        this.employeeSalary = employeeSalary;
+        count++;
+        this.id = count;
+    }
+
+    public String getEmployeeName() {
+        return this.employeeName;
+    }
+
+    public int getEmployeeDepartment() {
+        return this.employeeDepartment;
+    }
+
+    public double getEmployeeSalary() {
+        return this.employeeSalary;
+    }
+
+    public int getCount() {
+        return Employee.count;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public void setEmployeeDepartment(int employeeDepartment) {
+        this.employeeDepartment = employeeDepartment;
+    }
+
+    public void setEmployeeSalary(double employeeSalary) {
+        this.employeeSalary = employeeSalary;
+    }
+
+    public String toString() {
+        return id + "." + " ФИО - " + employeeName + "; Отдел - " + employeeDepartment + "; Зарплата - " + employeeSalary;
+    }
+
+    public static void info(Employee[] list) {
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i]);
+        }
+    }
+
+    public static void sum(Employee[] list) {
+        for (int i = 0; i < list.length; i++) {
+            SalarySum = SalarySum + list[i].getEmployeeSalary();
+        }
+        System.out.println("Сумма зарплат сотрудников - " + SalarySum);
+    }
+
+    public static void min(Employee[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getEmployeeSalary() < minSalary) {
+                minSalary = list[i].getEmployeeSalary();
+            }
+        }
+        System.out.println("Минимальная зарплата сотрудника - " + minSalary);
+    }
+
+    public static void max(Employee[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i].getEmployeeSalary() > maxSalary) {
+                maxSalary = list[i].getEmployeeSalary();
+            }
+        }
+        System.out.println("Максимальная зарплата сотрудника - " + minSalary);
+    }
+
+    public static void avg(Employee[] list) {
+        average = SalarySum / list.length;
+        System.out.println("Средняя зарплата сотрудников - " + average);
+    }
+
+    public static void name(Employee[] list) {
+        for (int i = 0; i < list.length; i++) {
+            System.out.println("ФИО сотрудника - " + list[i].getEmployeeName());
+        }
+    }
+}
+
+
--- src/KURS/Main.java
+++ src/KURS/Main.java
@@ -1,4 +1,7 @@
-import java.util.List;
+package KURS;
+
+import KURS.Employee;
+
 
 public class Main {
 
@@ -14,33 +17,12 @@ public class Main {
         list[7] = new Employee("Вишнякова Влада Якововна", 3, 123000);
         list[8] = new Employee("Зиновьев Аввакуум Иосифович", 4, 234000);
         list[9] = new Employee("Мишина Эстелла Георгиевна", 5, 246000);
-
-//        double minSalary = 999999999;
-//        double SalarySum = 0;
-//        double maxSalary = 0;
-//
-//        for (int i = 0; i < list.length; i++) {
-//            SalarySum = SalarySum + list[i].getEmployeeSalary();
-//        }
-//        for (int i = 0; i < list.length; i++) {
-//            if (list[i].getEmployeeSalary() < minSalary) {
-//                minSalary = list[i].getEmployeeSalary();
-//            }
-//        }
-//            for (int i = 0; i < list.length; i++) {
-//                if (list[i].getEmployeeSalary() > maxSalary) {
-//                    maxSalary = list[i].getEmployeeSalary();
-//                }
-//        }
-//        System.out.println(SalarySum);
-//        System.out.println(minSalary);
-//        System.out.println(maxSalary);
-//        System.out.println(SalarySum / 10);
-//        for (int i = 0; i < list.length; i++) {
-//            System.out.println(list[i].getEmployeeName());
-//        }
-
-
+        Employee.info(list);
+        Employee.sum(list);
+        Employee. min(list);
+        Employee.max(list);
+        Employee. avg(list);
+        Employee.name(list);
     }
 
 }
